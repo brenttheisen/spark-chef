8778f5232cf74d92998522c225e2760d1e62d789
Brent Theisen <brent@bantamlabs.com>
2014-04-21 15:40:14 -0500

    Added spark_mem attribute.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default[:spark] = {
   :checksum => 'F911EB740784A63AF5B84D0FFB73217B7640FA47F7CBAF7F1ABEFEAE05A99436B939CD5DAAC0EF84EE8F37DAC2877061A463D04352245EDB7CBD543C9E519531',
   :home => '/usr/local/spark',
   :username => 'spark',
+  :spark_mem => nil,
   :local_ip => nil,
   :mesos_native_library => nil,
   :java_opts => nil,
